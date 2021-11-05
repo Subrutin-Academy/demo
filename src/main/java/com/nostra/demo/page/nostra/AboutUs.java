3cdda678a86e256b98926b3d60b6a9bd7e439971
Tedy Tri Saputro <tedy.saputro@nostratech.com>
2021-11-05 10:04:45 +0700

    pertemuan 2

---
+++ src/main/java/com/nostra/demo/page/nostra/AboutUs.java
@@ -0,0 +1,20 @@
+package com.nostra.demo.page.nostra;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+import com.nostra.demo.page.AbstractBasePage;
+
+@Component
+public class AboutUs extends AbstractBasePage{
+	
+	@FindBy(xpath = "//*[@id=\"page-content\"]/div/h2")
+	public WebElement aboutUsText;
+
+	@Override
+	public boolean isAt() {
+		return this.wait.until((d)->this.aboutUsText.isDisplayed());
+	}
+
+}
